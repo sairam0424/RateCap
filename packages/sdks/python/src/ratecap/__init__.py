12eeab23da2c6823a49c34824521c2ba14e68d38
sairam0424 <uggesairam0000@gmail.com>
2026-08-31 10:53:34 +0530

    feat(sdk): add EstimateLLMCost/estimate_llm_cost helpers to both SDKs

--- packages/sdks/python/src/ratecap/__init__.py
+++ packages/sdks/python/src/ratecap/__init__.py
@@ -1,3 +1,4 @@
 from ratecap.client import AllowResult, Client, Ticket
+from ratecap.cost import estimate_llm_cost
 
-__all__ = ["AllowResult", "Client", "Ticket"]
+__all__ = ["AllowResult", "Client", "Ticket", "estimate_llm_cost"]
